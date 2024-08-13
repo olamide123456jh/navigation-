298e462482d238af2899c40af03a55dc45414af0
olamide123456jh <adeyanjurukayat82@gmail.com>
2024-08-13 12:01:18 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      display: [`group-focus`],
+      opacity: [`group-focus`],
+      inset: [`group-focus`],
+    },
+  },
+  plugins: [],
+}
+
